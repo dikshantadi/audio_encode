fd324712c0030f07b09a653e9742af47119c7efa
Dikshyant <adhikarybad@gmail.com>
2025-08-27 21:42:40 +0545

    this much for now

---
+++ audio-data-app/src/backend/audiopro.py
@@ -0,0 +1,9 @@
+import scipy
+import matplotlib.pyplot as plt 
+
+#defining
+fs = 44100       # Sampling frequency
+Tb = 0.1         # Bit duration in seconds
+f0 = 18500       # Frequency for bit 0 in Hz
+f1 = 19500       # Frequency for bit 1 in Hz 
+
--- audio-data-app/src/backend/enkode.py
+++ audio-data-app/src/backend/enkode.py
@@ -1,12 +1,13 @@
 #converting text into utf-8 binary format
 
-import scipy
-import matplotlib.pyplot as plt 
-
 def take_message():
-    txt = input ("Write message")
+    txt = input ("Write message : ")
     x = txt.encode()
 
+    bitstream = ''.join(format(byte, '08b') for byte in x)
     print(x)
+    print(bitstream)
+
+
 
 take_message()
